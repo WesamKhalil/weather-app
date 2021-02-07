9a1e1fed828e47f12b4bda270ced004da79c0a9b
WesamKhalil <66225664+WesamKhalil@users.noreply.github.com>
2021-02-07 00:32:58 +0000

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+module.exports = {
+    mode: "development",
+    entry: {
+        main: "./src/index.js"
+    },
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "dist"),
+        port: process.env.PORT || 3000,
+        open: true,
+        hot: true
+
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    }
+}
